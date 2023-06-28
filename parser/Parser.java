50e4ca21c9148bca9d257af1d554e1a2c2f230fa
Andrew De Neve <andrew@Andrews-Air.attlocal.net>
2023-06-28 10:15:55 -0700

    Parser done

--- parser/Parser.java
+++ parser/Parser.java
@@ -113,4 +113,102 @@ public class Parser {
     match(';');
     return stmt;
   }
+
+  Expr bool() throws IOException {
+    Expr x = join();
+    while( look.tag == Tag.OR) {
+      Token tok = look; move(); x = new Or(tok, x, join());
+    }
+    return x;
+  }
+  Expr join() throws IOException {
+    Expr x = equality();
+    while( look.tag == Tag.AND) {
+      Token tok = look; move(); x = new And(tok, x, equality());
+    }
+    return x;
+  }
+  Expr equality() throws IOException {
+    Expr x = rel();
+    while( look.tag == Tag.EQ || look.tag == Tag.NE) {
+      Token tok = look; move(); x = new Rel(tok, x, rel());
+    }
+    return x;
+  }
+  Expr rel() throws IOException {
+    Expr x = expr();
+    switch( look.tag ){
+      case '<': case Tag.LE: case Tag.GE: case '>':
+        Token tok = look; move(); return new Rel(tok, x, expr());
+      default:
+        return x;
+    }
+  }
+  Expr expr() throws IOException {
+    Expr x = term();
+    while( look.tag == '+' || look.tag == '-'){
+      Token tok = look;, move(); x = new Arith(tok, x, term());
+    }
+    return x;
+  }
+  Expr term() throws IOException {
+    Expr x = unary();
+    while(look.tag == '*' || look.tag == '/') {
+      Token tok = look; move(); x = new Arith(tok, x, unary());
+    }
+    return x;
+  }
+  Expr unary() throws IOException {
+    if( look.tag == '-') {
+      move(); return new Unary(Word.minus, unary());
+    }
+    else if( look.tag == '!') {
+      Token tok = look; move(); return new Not(tok, unary());
+    }
+    else return factor();
+  }
+  Expr factor() throws IOException {
+    Expr x = null;
+    switch( look.tag ) {
+      case '(':
+        move(); x = bool(); match(')');
+        return x;
+      case Tag.NUM:
+        x = new Constant(look, Type.Int); move(); return x;
+      case Tag.REAL:
+        x = new Constant(look, Type.Float); move(); return x;
+      case Tag.TRUE:
+        x = Constant.True; move(); return x;
+      case Tag.FALSE:
+        x = Constant.False; move(); return x;
+      default:
+        error("syntax error");
+        return x;
+      case Tag.ID:
+        String s = look.toString();
+        Id id = top.get(look);
+        if( id == null) error(look.toString() + " undeclared");
+        move();
+        if( look.tag != '[') return id;
+        else return offset(id);
+    }
+  }
+  Access offset(Id a) throws IOException {
+    Expr i; Expr w; Expr t1, t2; Expr loc;
+    Type type = a.type;
+    match('['); i = bool(); match(']');
+    type = ((Array)type).of;
+    w = new Constant(type.width);
+    t1 = new Arith(new Token('*'), i, w);
+    loc = t1;
+    while( look.tag == '[') {   // multi-dimensional
+      match('['); i = bool(); match(']');
+      type = ((Array)type).of;
+      w = new Constant(type.width);
+      t1 = new Arith(new Token('*'), i, w);
+      t2 = new Arith(new Token('+'), loc, t1);
+      loc = t2;
+    }
+    return new Access(a, loc, type);
+  }
 }
